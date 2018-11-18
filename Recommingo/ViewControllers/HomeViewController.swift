3bd64ecec4181923711716fd76d10f699fd96edb
graemerenfrew <graemerenfrew+github@gmail.com>
2018-11-18 19:49:22 +0100

    Posts are displayed ok on the Home. Rearranged the tab bar

--- Recommingo/ViewControllers/HomeViewController.swift
+++ Recommingo/ViewControllers/HomeViewController.swift
@@ -12,43 +12,51 @@ import FirebaseAuth
 import FirebaseDatabase
 
 class HomeViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
+    var posts = [Post]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self //this calls the extension below
         loadPosts()
+        
         //create a Post instance object
-        var post = Post(captionText: "Test", photoUrlString: "URL")
-        print(post.caption)
-        print(post.photoUrl)
+        //var post = Post(captionText: "Test", photoUrlString: "URL")
+        //print(post.caption)
+        //print(post.photoUrl)
+        //TODO Remove
     }
     
     func loadPosts() {
         Database.database().reference().child("posts").observe(.childAdded) { (snapshot: DataSnapshot) in
-            
+            if let dict = snapshot.value as? [String: Any] {
+                    //create post instance from dictionary key:value pair
+                    let newPost = Post.transformPost(dict: dict)
+                
+                //append the latest snapshot into an array of posts
+                self.posts.append(newPost)
+                self.tableView.reloadData()
+            }
         }
     }
     
     @IBAction func logout_TouchUpInside(_ sender: Any) {
-         do {
-                try Auth.auth().signOut()
-            } catch let logoutError {
-                print(logoutError)
-                }
+        do {
+            try Auth.auth().signOut()
+        } catch let logoutError {
+            print(logoutError)
+        }
         let storyboard:UIViewController = UIStoryboard(name:"Start", bundle:nil).instantiateViewController(withIdentifier: "SignInViewController") as UIViewController
         self.present(storyboard, animated: true, completion: nil)
- 
+        
     }
     
-   
     
-
 }
 extension HomeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -57,8 +65,10 @@ extension HomeViewController: UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath)
         
         //alternate
-        cell.textLabel?.text = "\(indexPath.row)"
-        cell.backgroundColor = .red
+        //cell.textLabel?.text = "\(indexPath.row)"
+        cell.textLabel?.text = posts[indexPath.row].caption
+        
+        //cell.backgroundColor = .red
         return cell
     }
     
